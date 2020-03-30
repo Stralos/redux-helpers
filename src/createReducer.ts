075a41b26ca4fee8f66e569f41cd0102d9e3516c
Paulius Kiselis <paulius.kiselis@wahanda.com>
2020-03-30 22:01:12 +0300

    Implement createReducer functionality

--- src/createReducer.ts
+++ src/createReducer.ts
@@ -1,7 +1,12 @@
 import { On } from "./on";
+import { Action } from "./createAction";
 
 export const createReducer = <S>(initialState: S, ...args: On<S>[]) => {
-  return (state = initialState): S => {
+  return <T>(state = initialState, action: Action<T>): S => {
+    const reducer = args.find(reducer => reducer.type.includes(action.type));
+    if (reducer) {
+      return reducer.reducer(state, action.payload);
+    }
     return state;
   };
 };
